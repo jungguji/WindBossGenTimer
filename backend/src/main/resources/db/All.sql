35b68a7c5d1c4adbc762b2696fb948da717ef559
junggu <junggu.ji.dev@gmail.com>
2020-09-03 19:22:14 +0900

    type : fix
    db 구조 변경
    
    젠타임 one
    던전 many
    젠타임 -> 던전

--- backend/src/main/resources/db/All.sql
+++ backend/src/main/resources/db/All.sql
@@ -4,7 +4,7 @@
 CREATE TABLE wind_boss_gen_timer.user
 (
     `id`        INT             NOT NULL    AUTO_INCREMENT COMMENT 'id',
-    `email`     VARCHAR(500)    NOT NULL    COMMENT '이메일',
+    `email`     VARCHAR(500)    NOT NULL    COMMENT '메일',
     `password`  VARCHAR(200)    NOT NULL    COMMENT '비밀번호',
     PRIMARY KEY (id)
 );
@@ -13,37 +13,37 @@ ALTER TABLE wind_boss_gen_timer.user COMMENT '유저';
 
 
 -- user Table Create SQL
-CREATE TABLE wind_boss_gen_timer.gen_time
-(
-    `id`     INT            NOT NULL    AUTO_INCREMENT COMMENT 'id',
-    `보스 이름`  VARCHAR(80)    NULL        COMMENT 'boss_name',
-    `젠 타임`   TIME           NULL        COMMENT 'gen_time',
-    PRIMARY KEY (id)
-);
-
-ALTER TABLE wind_boss_gen_timer.gen_time COMMENT '보스 별 젠 타임';
-
-
--- user Table Create SQL
 CREATE TABLE wind_boss_gen_timer.dungeon
 (
-    `아이디`       INT     NOT NULL    AUTO_INCREMENT COMMENT 'id',
-    `유저 아이디`    INT     NOT NULL    COMMENT 'user_id',
-    `젠타임 아이디`   INT     NOT NULL    COMMENT 'gen_time_id',
-    `주 채널`      INT     NULL        COMMENT 'main_channel',
-    `서브 채널`     INT     NULL        COMMENT 'sub_channel',
-    `보스 죽인 시간`  TIME    NULL        COMMENT 'kill_time',
-    PRIMARY KEY (아이디)
+    `id`           INT     NOT NULL    AUTO_INCREMENT COMMENT 'id',
+    `user_id`       INT     NOT NULL    COMMENT '유저 id',
+    `main_channel`  INT     NULL        COMMENT '주 채널',
+    `sub_channel`   INT     NULL        COMMENT '서브 채널',
+    `kill_time`     TIME    NULL        COMMENT '보스 죽인 시간',
+    PRIMARY KEY (id)
 );
 
 ALTER TABLE wind_boss_gen_timer.dungeon COMMENT '던전';
 
 ALTER TABLE wind_boss_gen_timer.dungeon
-    ADD CONSTRAINT FK_dungeon_유저 아이디_user_id FOREIGN KEY (유저 아이디)
+    ADD CONSTRAINT FK_dungeon_user_id_user_id FOREIGN KEY (user_id)
         REFERENCES wind_boss_gen_timer.user (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-ALTER TABLE wind_boss_gen_timer.dungeon
-    ADD CONSTRAINT FK_dungeon_젠타임 아이디_gen_time_id FOREIGN KEY (젠타임 아이디)
-        REFERENCES wind_boss_gen_timer.gen_time (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- user Table Create SQL
+CREATE TABLE wind_boss_gen_timer.gen_time
+(
+    `id`          INT            NOT NULL    AUTO_INCREMENT COMMENT 'id',
+    `boss_name`   VARCHAR(80)    NULL        COMMENT '보스 이름',
+    `gen_time`    TIME           NULL        COMMENT '젠 타임',
+    `dungeon_id`  INT            NULL        COMMENT '던전 아이디',
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE wind_boss_gen_timer.gen_time COMMENT '보스 별 젠 타임';
+
+ALTER TABLE wind_boss_gen_timer.gen_time
+    ADD CONSTRAINT FK_gen_time_dungeon_id_dungeon_아이디 FOREIGN KEY (dungeon_id)
+        REFERENCES wind_boss_gen_timer.dungeon (아이디) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 
--- backend/src/main/resources/db/gen_time.sql
+++ backend/src/main/resources/db/gen_time.sql
@@ -1,9 +1,9 @@
--- user Table Create SQL
 CREATE TABLE wind_boss_gen_timer.gen_time
 (
-    `id`         INT            NOT NULL    AUTO_INCREMENT COMMENT 'id',
-    `boss_name`  VARCHAR(80)    NULL        COMMENT '보스 이름',
-    `gen_time`   TIME           NULL        COMMENT '젠 타임',
+    `id`          INT            NOT NULL    AUTO_INCREMENT COMMENT 'id',
+    `boss_name`   VARCHAR(80)    NULL        COMMENT '보스 이름',
+    `gen_time`    TIME           NULL        COMMENT '젠 타임',
+    `dungeon_id`  INT            NULL        COMMENT '던전 아이디',
     PRIMARY KEY (id)
 );
 
