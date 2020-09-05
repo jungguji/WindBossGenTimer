e69865fac8740c5165ee8863b7f2a511e5563e09
junggu <junggu.ji.dev@gmail.com>
2020-09-05 19:43:05 +0900

    type : feat
    builder 패턴 추가

--- backend/src/main/java/com/jungguji/windbossgentimer/domain/gentime/GenTime.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/domain/gentime/GenTime.java
@@ -1,14 +1,19 @@
 package com.jungguji.windbossgentimer.domain.gentime;
 
 import com.jungguji.windbossgentimer.domain.dungeon.Dungeon;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import org.springframework.util.Assert;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalTime;
 
 @Getter
-@RequiredArgsConstructor
 @Entity
 public class GenTime {
 
@@ -21,4 +26,15 @@ public class GenTime {
     @ManyToOne
     @JoinColumn(name="dungeon_id")
     private final Dungeon dungeon;
+
+    @Builder
+    public GenTime(String bossName, LocalTime genTime, Dungeon dungeon) {
+        Assert.hasText(bossName, "boss name is required");
+        Assert.notNull(genTime, "gen time is required");
+        Assert.notNull(dungeon, "dungeon is required");
+
+        this.bossName = bossName;
+        this.genTime = genTime;
+        this.dungeon = dungeon;
+    }
 }
