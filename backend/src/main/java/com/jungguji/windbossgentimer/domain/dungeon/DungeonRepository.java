61bdddcb23e0649f9c35eab386f435ea35eda29a
junggu <junggu.ji.dev@gmail.com>
2020-09-07 20:16:03 +0900

    type : feat
    유저, 던전 레파지토리 생성

---
+++ backend/src/main/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepository.java
@@ -0,0 +1,6 @@
+package com.jungguji.windbossgentimer.domain.dungeon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DungeonRepository extends JpaRepository<Dungeon, Long> {
+}
