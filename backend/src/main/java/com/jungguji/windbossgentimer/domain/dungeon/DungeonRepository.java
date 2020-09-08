7d48f2cc955014ec5a7a29c18bb49c21ae74d3e7
junggu <junggu.ji.dev@gmail.com>
2020-09-08 18:42:39 +0900

    type : feat
    User별 던전 리스트 메서드, 던전 별 메인 채널 리스트 메서드 추가

--- backend/src/main/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepository.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepository.java
@@ -1,6 +1,15 @@
 package com.jungguji.windbossgentimer.domain.dungeon;
 
+import com.jungguji.windbossgentimer.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DungeonRepository extends JpaRepository<Dungeon, Long> {
+
+    List<Dungeon> findByUser(User user);
+
+    @Query(value = "SELECT mainChannel FROM Dungeon WHERE name =:name")
+    List<Integer> findMainChannelByName(String name);
 }
