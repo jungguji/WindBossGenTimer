c90ef94a570f12955b854f15bab2cd58e0f4d1e7
junggu <junggu.ji.dev@gmail.com>
2020-09-08 18:42:08 +0900

    type : test
    유저별 던전 리스트, 던전 메인채널 리스트 test

--- backend/src/test/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepositoryTest.java
+++ backend/src/test/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepositoryTest.java
@@ -59,9 +59,25 @@ class DungeonRepositoryTest {
                 .user(user)
                 .build();
 
+        Dungeon dungeon3 = Dungeon.builder()
+                .name(dungeonName2)
+                .mainChannel(2)
+                .subChannel(1)
+                .user(user)
+                .build();
+
+        Dungeon dungeon4 = Dungeon.builder()
+                .name(dungeonName2)
+                .mainChannel(3)
+                .subChannel(11)
+                .user(user)
+                .build();
+
         dungeons = Arrays.asList(
                 dungeon1
                 , dungeon2
+                , dungeon3
+                , dungeon4
         );
 
         dungeonRepository.saveAll(dungeons);
@@ -85,7 +101,58 @@ class DungeonRepositoryTest {
             assertEquals(given.getSubChannel(), when.getSubChannel());
             assertEquals(given.getUser().getEmail(), when.getUser().getEmail());
         }
+    }
 
+    @Test
+    void 유저별_던전_리스트_가져오기() {
+        //given
+        String user1Email = "jgji@gmail.com";
+        String user1pw = "test123";
 
+        User user1 = User.builder()
+                .email(user1Email)
+                .password(user1pw)
+                .build();
+
+        userRepository.save(user1);
+
+        String dname = "비밀세작의 집";
+        int mc = 20;
+        int sm = 9;
+
+        Dungeon given = Dungeon.builder()
+                .name(dname)
+                .mainChannel(mc)
+                .subChannel(sm)
+                .user(user1)
+                .build();
+
+        dungeonRepository.save(given);
+
+        //when
+        List<Dungeon> whens = dungeonRepository.findByUser(user1);
+
+        //then
+        assertEquals(1, whens.size());
+
+        Dungeon when = whens.get(0);
+
+        assertEquals(given.getName(), when.getName());
+        assertEquals(given.getMainChannel(), when.getMainChannel());
+        assertEquals(given.getSubChannel(), when.getSubChannel());
+        assertEquals(given.getUser().getEmail(), when.getUser().getEmail());
+    }
+
+    @Test
+    void 던전_별_메인채널_리스트_가져오기() {
+        //given
+        //when
+        List<Integer> whens = dungeonRepository.findMainChannelByName(dungeonName2);
+
+        //then
+        assertEquals(3, whens.size());
+        assertEquals(1, whens.get(0));
+        assertEquals(2, whens.get(1));
+        assertEquals(3, whens.get(2));
     }
 }
