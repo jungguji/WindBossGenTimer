9e6e150755574881f3023694da2e6009aa804db2
junggu <junggu.ji.dev@gmail.com>
2020-09-08 18:43:02 +0900

    type : feat
    Gen time repository 추가

---
+++ backend/src/main/java/com/jungguji/windbossgentimer/domain/gentime/GenTimeRepository.java
@@ -0,0 +1,7 @@
+package com.jungguji.windbossgentimer.domain.gentime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenTimeRepository extends JpaRepository<GenTime, Long> {
+
+}
