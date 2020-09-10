c1a45985000184a653f6398d69a790268a58cf76
JG.JI <junggu.ji.dev@gmail.com>
2020-09-10 23:37:11 +0900

    type : feat
    @Repository 어노테이션 추가

--- backend/src/main/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepository.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/domain/dungeon/DungeonRepository.java
@@ -3,9 +3,11 @@ package com.jungguji.windbossgentimer.domain.dungeon;
 import com.jungguji.windbossgentimer.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DungeonRepository extends JpaRepository<Dungeon, Long> {
 
     List<Dungeon> findByUser(User user);
