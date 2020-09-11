17e0b1ac1bc517bdf262cb09a7f4b27a2a34dd4b
JG.JI <junggu.ji.dev@gmail.com>
2020-09-11 23:26:44 +0900

    type : feat
    UTF-8 설정 추가

--- backend/src/main/java/com/jungguji/windbossgentimer/WindBossGenTimerApplication.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/WindBossGenTimerApplication.java
@@ -2,6 +2,12 @@ package com.jungguji.windbossgentimer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class WindBossGenTimerApplication {
@@ -10,4 +16,16 @@ public class WindBossGenTimerApplication {
 		SpringApplication.run(WindBossGenTimerApplication.class, args);
 	}
 
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
