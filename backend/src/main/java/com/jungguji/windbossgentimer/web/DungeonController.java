593a0d79d1ce845779dd1407e02ffb4a52919cd1
JG.JI <junggu.ji.dev@gmail.com>
2020-09-12 12:41:09 +0900

    #2 type : feat
    @RestController로 변경 및 던전명 list 가져오는 메서드로 변경

--- backend/src/main/java/com/jungguji/windbossgentimer/web/DungeonController.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/web/DungeonController.java
@@ -3,20 +3,18 @@ package com.jungguji.windbossgentimer.web;
 import com.jungguji.windbossgentimer.service.DungeonService;
 import com.jungguji.windbossgentimer.web.dto.DungeonDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class DungeonController {
     private final DungeonService dungeonService;
 
-    @GetMapping(value="/all", produces = "application/json")
-    @ResponseBody
-    public List<DungeonDTO.MainView> findAll() {
-        return DungeonDTO.MainView.toMainViewList(dungeonService.findAll());
+    @GetMapping(value="/main", produces = "application/json")
+    public List<DungeonDTO.MainView> findNameByGroupName() {
+        return DungeonDTO.MainView.toMainViewList(dungeonService.findNameByGroupName());
     }
 }
