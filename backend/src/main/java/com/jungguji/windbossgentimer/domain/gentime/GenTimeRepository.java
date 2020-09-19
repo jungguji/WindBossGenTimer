9e830035bfe349115fa55778ad4694be1a385a20
JG.JI <junggu.ji.dev@gmail.com>
2020-09-19 15:52:58 +0900

    type : feat
    테이블 구조 변경
    
    유저 1 : n 던전
    지역 1 : n 던전
    던전 1 : n 보스
    던전 1 : n 채널
    보스 1 : n 죽인 시간
    채널 1 : n 죽인 시간

--- backend/src/main/java/com/jungguji/windbossgentimer/domain/gentime/GenTimeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jungguji.windbossgentimer.domain.gentime;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenTimeRepository extends JpaRepository<GenTime, Long> {
-
-}
