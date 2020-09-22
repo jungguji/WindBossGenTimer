83ca508c2f8f18f1919db743f33c5de560225163
junggu <junggu.ji.dev@gmail.com>
2020-09-22 21:32:48 +0900

    #3 type : refactor
    import split

--- backend/src/main/java/com/jungguji/windbossgentimer/domain/region/Region.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/domain/region/Region.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
