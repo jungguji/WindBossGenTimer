0488f011c2fe8d72163728aa5df3f886caa95f68
junggu <junggu.ji.dev@gmail.com>
2020-09-24 17:04:27 +0900

    #13 type : feat
    cors 설정 common으로 뺌

--- frontend/src/apis/main_api.js
+++ frontend/src/apis/main_api.js
@@ -1,15 +1,7 @@
-import axios from "axios";
-
-const AXIOS = axios.create({
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json; charset = utf-8"
-  },
-  timeout: 1000
-});
+import { AXIOS } from "./http_common";
 
 export default {
   requestMainView() {
     return AXIOS.get(`${process.env.VUE_APP_BASEURL}/main`);
   }
-}
+};
