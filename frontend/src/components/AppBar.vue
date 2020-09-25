ab08c928427449f08b5e012571c93c545b8d1b10
junggu <junggu.ji.dev@gmail.com>
2020-09-25 20:38:46 +0900

    #13 type : feat
    네비게이션 및 왼쪽 side 바에 던전 리스트 나오도록 수정

---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,135 @@
+<template>
+  <nav>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row v-if="item.heading" :key="item.heading" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+            v-else-if="item.dungeons"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ item.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item
+              v-for="dungeon in item.dungeons"
+              :key="dungeon.name"
+              @click="popupChannel(1)"
+            >
+              <v-icon> mdi-cog </v-icon>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ dungeon.name }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else :key="item.text" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">Google Contacts</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" item>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          ></v-img
+        ></v-avatar>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+import api from "../apis/main_api.js";
+
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      {
+        icon: "mdi-chevron-up",
+        "icon-alt": "mdi-chevron-down",
+        text: "던전 리스트",
+        model: false,
+        dungeons: []
+      }
+    ]
+  }),
+  methods: {
+    popupChannel(id) {
+      this.$router.push(`/dungeon/${id}`);
+    }
+  },
+  mounted() {
+    api
+      .requestMainView()
+      .then(response => {
+        this.items[0].dungeons = response.data;
+        console.log(response.data);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  }
+};
+</script>
