e5dbc387598175487d7380a05dd65a44f9fa707f
junggu <junggu.ji.dev@gmail.com>
2020-09-27 21:12:42 +0900

    #3 type : refactor
    불필요한 메서드 삭제

--- backend/src/main/java/com/jungguji/windbossgentimer/web/DungeonController.java
+++ backend/src/main/java/com/jungguji/windbossgentimer/web/DungeonController.java
@@ -1,11 +1,7 @@
 package com.jungguji.windbossgentimer.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
