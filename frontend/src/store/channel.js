a96c6dd2d91a66df35874e1f4c4b415e8c4ecf6f
junggu <junggu.ji.dev@gmail.com>
2020-10-05 20:57:18 +0900

    #4 type : refactor
    콘솔로그 제거

--- frontend/src/store/channel.js
+++ frontend/src/store/channel.js
@@ -13,11 +13,7 @@ const mutations = {
 const actions = {
   async QUERY_BOSS(context, params) {
     try {
-      console.log("actions");
-      console.log(params.dungeonId);
-      console.log(params.channelId);
       const response = await requestBoss(params);
-      console.log(response.data);
       context.commit("GET_BOSS_LIST", response.data);
       return response.data;
     } catch (e) {
